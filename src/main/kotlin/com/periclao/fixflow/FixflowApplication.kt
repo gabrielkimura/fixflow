94df0060137b176059e7e81ea3c660905ac68823
gabrielkimura <gabrielkimura7@gmail.com>
2026-05-26 12:46:25 -0300

    first commit

---
+++ src/main/kotlin/com/periclao/fixflow/FixflowApplication.kt
@@ -0,0 +1,11 @@
+package com.periclao.fixflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FixflowApplication
+
+fun main(args: Array<String>) {
+	runApplication<FixflowApplication>(*args)
+}
---
+++ src/test/kotlin/com/periclao/fixflow/FixflowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.periclao.fixflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FixflowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
